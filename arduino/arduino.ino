dbabb67e2c8c7317b1e4c0893a6d6d021c52b5df
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2021-10-19 22:46:17 +0200

    ardino logic added

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,9 +1,52 @@
-void setup() {
-  // put your setup code here, to run once:
+#define red_led 9
+#define blue_led 10
 
+boolean red = false;
+boolean blue = false;
+
+int red_speed;
+int blue_speed;
+
+void setup()
+{
+  pinMode(red_led, OUTPUT);
+  pinMode(blue_led, OUTPUT);
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  if (Serial.available()){
+    char led_specifier = Serial.read();
+    int led_blink_speed = Serial.parseInt();
+    write_leds(led_specifier, led_blink_speed);
+  }
+}
 
+void write_leds(char led, int speedd)
+{
+  if (led == 'r'){
+    red = true;
+    red_speed = speedd;
+  }
+  if (red == true){
+    digitalWrite(red_led,HIGH);
+    delay(red_speed);
+    digitalWrite(red_led,LOW);
+    delay(red_speed);
+  }
+  
+  if (led == 'b'){
+    blue = true;
+    blue_speed = speedd;
+  }
+  
+  if (blue == true){
+    digitalWrite(blue_led,HIGH);
+    delay(blue_speed);
+    digitalWrite(blue_led,LOW);
+    delay(blue_speed);
+  }
+  
+  return;
 }
