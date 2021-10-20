2ebb27dadbabec4a25ddb9b82199b25675dd3193
Islam231bi <47501442+Islam231bi@users.noreply.github.com>
2021-10-20 03:44:24 +0200

    Update arduino.ino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,9 +1,44 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
 
+*/
+
+#define red_led 3
+#define blue_led 4
+
+char led;
+int speed;
+
+void setup() {
+  
+  pinMode(red_led,OUTPUT);
+  pinMode(blue_led,OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+    if (Serial.available()) {
+      
+      led = Serial.read();
+      speed = Serial.parseInt();    
+    }
+    
+    if (led == 'R')
+      blinkyBlinky(red_led,speed);
+    else if (led == 'B')
+      blinkyBlinky(blue_led,speed);
+    else{ 
+      digitalWrite(red_led,LOW);
+      digitalWrite(blue_led,LOW);
+    }
+}
 
+void blinkyBlinky(int LED, int speed)
+{
+  
+    digitalWrite(LED, HIGH);
+    delay(speed);
+    digitalWrite(LED, LOW);
+    delay(speed);
+  
 }
