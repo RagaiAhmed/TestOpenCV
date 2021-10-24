ad4d3c2c7e3cf3a89ed20c50bbff11a6cde30676
Hythem-shaban <89210658+Hythem-shaban@users.noreply.github.com>
2021-10-24 23:27:48 +0200

    add qt files

---
+++ qtserialport/untitled5/dialog.h
@@ -0,0 +1,34 @@
+#ifndef DIALOG_H
+#define DIALOG_H
+
+#include <QDialog>
+#include <QtSerialPort>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Dialog; }
+QT_END_NAMESPACE
+
+class Dialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    Dialog(QWidget *parent = nullptr);
+    ~Dialog();
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void updateleds(QString);
+
+private:
+    Ui::Dialog *ui;
+    QSerialPort *arduino;
+    static const quint16 arduino_uno_vendor_id = 9025;
+    static const quint16 arduino_uno_product_id = 67;
+    QString arduino_port_name;
+    bool arduino_is_available;
+};
+#endif // DIALOG_H
