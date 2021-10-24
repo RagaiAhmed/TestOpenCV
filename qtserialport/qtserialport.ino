f8968e90af159ffa5f2ab690fb5abb6e77556aae
Hythem-shaban <89210658+Hythem-shaban@users.noreply.github.com>
2021-10-24 22:58:27 +0200

    Update qtserialport.ino
    update arduino code to solve conflicts

--- qtserialport/qtserialport.ino
+++ qtserialport/qtserialport.ino
@@ -1,49 +1,24 @@
 #define red_led 9
 #define blue_led 10
-<<<<<<< Updated upstream
 void setup() {
   pinMode(red_led, OUTPUT);
   pinMode(blue_led, OUTPUT);
-=======
-int red_blinking_speed;
-int blue_blinking_speed;
-int previous_time_red = 0;
-int previous_time_blue = 0;
+  int red_blinking_speed;
+  int blue_blinking_speed;
+  int previous_time_red = 0;
+  int previous_time_blue = 0;
+
 void setup() {
 
   pinMode(red_led, OUTPUT);
   pinMode(blue_led, OUTPUT);
   digitalWrite(red_led, LOW);
   digitalWrite(blue_led, LOW);
->>>>>>> Stashed changes
 
 }
 
 void loop() {
-<<<<<<< Updated upstream
-  if (Serial.available()){
-    char led_specifier = Serial.read();
-    int led_delay = Serial.parseInt();
-    write_leds(led_specifier, led_delay);
 
-}
-void write_leds(char led, int blinking)
-{
-  if (led == 'S'){
-    digitalWrite(red_led,LOW);
-    digitalWrite(blue_led,LOW);
-  }
-  if (led == 'R'){
-    digitalWrite(red_led,HIGH);
-    delay(blinking);
-    digitalWrite(red_led,LOW);
-  }
-  if (led == 'B'){
-    digitalWrite(blue_led,HIGH);
-    delay(blinking);
-    digitalWrite(blue_led,LOW);
-  }
-=======
   int current_time = millis();
   char led_specifier;
   int led_blinking_speed;
@@ -72,5 +47,3 @@ void write_leds(char led, int blinking)
   }
   
 }
-
->>>>>>> Stashed changes
